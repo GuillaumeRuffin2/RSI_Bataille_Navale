59067784601c5e5265f5ba424272f3d01ef4e34c
IMBEAUD Aymeric <aimbeaud@slalom.pedago.ipb.fr>
2020-02-05 12:18:34 +0100

    prog début

--- project-1.c
+++ project-1.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define N 10
+int tab[N][N];
+
+struct batal {
+
+}
+void remplir_tableau(struct batal){
+    
+}
 
 int main (int argc, *char[] argv) {
     printf("test\n");
